0acd7b7f4acc92ae53bc7c08f9c8d9b1f3aaddd3
Md Shahriar Khan <77568415+ShahriarKhan099@users.noreply.github.com>
2022-04-02 00:38:24 +0600

    Add files via upload

---
+++ user-schema.sql
@@ -0,0 +1,15 @@
+-- create table usermanagement_tut.user (
+-- 	               id INT NOT NULL AUTO_INCREMENT,
+-- 				   name VARCHAR(45) NOT NULL,
+-- 				   address VARCHAR(75) NOT NULL,
+-- 				   phone VARCHAR(45) NOT NULL,
+--                    license VARCHAR(45) NOT NULL,
+-- 				   car_engine VARCHAR(45) NOT NULL,
+--                    appointment_date DATE NOT NULL,
+--                    mechanics VARCHAR(45) NOT NULL,
+--                      status VARCHAR(10) NOT NULL DEFAULT 'active',
+-- 				   PRIMARY KEY (`id`)) ENGINE = InnoDB;
+	               
+-- INSERT INTO user 
+-- (id, name, address, phone, license, car_engine, appointment_date, mechanics, status) VALUES
+-- (1, 'Amanda Naina', 'new york', '+1231331',  '012345 678910', 'petrol gate',  '2021-11-11', 'Karim mia', 'active')
